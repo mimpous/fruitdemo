30179d152b983f385f2a8ab70d264fd910e4150a
Bousios Michail <mimpous@gmail.com>
2020-08-18 17:23:47 +0200

    first commit

---
+++ src/main/java/com/waya/fruitdemo/FruitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.waya.fruitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waya/fruitdemo/FruitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waya.fruitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
